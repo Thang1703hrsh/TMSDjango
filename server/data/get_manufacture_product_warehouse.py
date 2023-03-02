acad936aa247e68fc98005f2caba01e1de3b46ea
Thang Tran <ducthang170301@gmail.com>
2023-03-02 10:27:12 +0700

    get_manufacture_warehouse

---
+++ server/data/get_manufacture_product_warehouse.py
@@ -0,0 +1,13 @@
+from .models_data import *
+import pandas as pd
+
+## Outsourcing Stock In Detail
+dfManuProdWarehouse = ProductDetail.objects.raw("""  
+        SELECT *
+        FROM manufacture_product_warehouse MPW
+        WHERE MPW.deleted_at IS NULL
+        """)
+
+dfManuProdWarehouse = pd.DataFrame([item.__dict__ for item in dfManuProdWarehouse]).drop(['_state'], axis=1)
+print(dfManuProdWarehouse)
+dfManuProdWarehouse.to_csv('../data_csv/ManuProdWarehouse.csv', index= False)
